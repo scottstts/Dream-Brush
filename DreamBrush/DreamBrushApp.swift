0a257be996024f72f53b5be91cb770e4c59a5651
scott <st.scott0612@gmail.com>
2026-01-05 19:19:18 +0000

    Initial Commit

---
+++ DreamBrush/DreamBrushApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamBrushApp.swift
+//  DreamBrush
+//
+//  Created by Scott Sun on 2026/1/5.
+//
+
+import SwiftUI
+
+@main
+struct DreamBrushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DreamBrushTests/DreamBrushTests.swift
@@ -0,0 +1,17 @@
+//
+//  DreamBrushTests.swift
+//  DreamBrushTests
+//
+//  Created by Scott Sun on 2026/1/5.
+//
+
+import Testing
+@testable import DreamBrush
+
+struct DreamBrushTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
